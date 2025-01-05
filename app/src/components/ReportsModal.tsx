3d10303d255515e007ea101a4324f981b838e1fa
itaybober2 <itaybober2@gmail.com>
2025-01-05 12:00:24 +0200

    itay-backend (#7)
    * itay-backend (#5) (#6)
    
    * pushing initial server.js
    
    * trying to connect backend
    
    ---------
    
    Co-authored-by: boberitay <140694250+boberitay@users.noreply.github.com>
    
    * backend connected! 🚀
    
    ---------
    
    Co-authored-by: boberitay <140694250+boberitay@users.noreply.github.com>

--- app/src/components/ReportsModal.tsx
+++ app/src/components/ReportsModal.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import './ReportsModal.css';
 import {useEffect, useState} from "react";
-import {fetchReports, Report} from "@/backend/utils/api";
+import {fetchReports} from "@/backend/utils/api";
 
 const style = {
     position: 'absolute',
@@ -20,17 +20,30 @@ type ModalProps = {
     open: boolean;
     onClose: () => void;
 };
+
+export type Report = {
+    id: number;
+    content: string;
+    created_at: string | null;
+};
+
+
 export default function ReportsModal(props: ModalProps) {
     const { open, onClose } = props;
     const [reports, setReports] = useState<Report[]>([]);
 
     useEffect(() => {
-        const fetchData = async () =>  {
-            const data = await fetchReports();
-            setReports(data);
-        }
+        const fetchData = async () => {
+            try {
+                const data = await fetchReports();
+                setReports(data.data);
+            } catch (error) {
+                console.error("Error fetching reports:", error);
+            }
+        };
         fetchData();
     }, []);
+
 
     return (
         <div>
