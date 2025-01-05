7e4800d8d233c7a81252c6acd7950c629197e825
boberitay <140694250+boberitay@users.noreply.github.com>
2025-01-05 11:43:07 +0200

    trying to connect backend

--- backend/utils/api.ts
+++ backend/utils/api.ts
@@ -14,6 +14,7 @@ export async function fetchReports() {
     return response.json();
 }
 
+
 export async function createReport(content: string) {
     const response = await fetch(`${backendUrl}/reports`, {
         method: "POST",
