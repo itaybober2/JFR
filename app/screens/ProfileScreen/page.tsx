fcd1b5c015262488d93a3fb2900d770e7bd8dc1f
yardenross <yarden.ross@mail.huji.ac.il>
2025-01-26 11:33:03 +0200

    added profile page

---
+++ app/screens/ProfileScreen/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import "@/app/screens/ProfileScreen/Profile.css";
+import { useState } from "react";
+import Navbar from "@/lib/components/Navbar";
+
+
+export default function ProfileScreen() {
+    const [toMountScoupe, setToMountScoupe] = useState(true); 
+    
+
+  return (
+    <main className="profilePage">
+        <Navbar toMountScoupe={toMountScoupe} setToMountScoupe={setToMountScoupe} />
+        <div className="profile">
+            <div className="userNameCircle"></div>
+            <div className="userInfo">
+                <div className="userName">שם משתמש</div>
+                <div className="email">userName@gmail.com</div>
+            </div>
+        </div>
+
+        <div className="menu">
+            <button className="menuItem">שינוי היעדים שלי</button>
+            <button className="menuItem">צפייה בנקודות שלי</button>
+            <button className="menuItem">הגדרות פרטיות</button>
+      </div>
+    </main>
+  );
+}
